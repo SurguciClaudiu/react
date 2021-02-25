fd455dced79c8742947417dd0e5d7b576b7ff17c
SurguciClaudiu <surguci.claudiu@gmail.com>
2021-02-25 22:33:07 +0200

    ELENA

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         MERGEEEEEEEEEEEEEEEEEE
+         ELENAAAA
         </p>
         <a
           className="App-link"
