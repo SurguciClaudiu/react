0a2a20c86084b7a3787c818f1f13afd92b478f2f
SurguciClaudiu <surguci.claudiu@gmail.com>
2021-02-25 22:40:59 +0200

    modified app.js - added elena

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         ELENAAAA
+         Elena Plangacioasa
         </p>
         <a
           className="App-link"
